1e0a614d6d476fcbb269d9f86cea0454df72ea42
Rohan Mehta <rohanmehta@Rohans-MacBook-Air.local>
2020-05-01 19:16:30 -0500

    initial commit

---
+++ sample_config.py
@@ -0,0 +1,10 @@
+# Alpaca Keys
+API_KEY = "yourpaperapikey"
+SECRET_KEY = "yourpapersecretkey"
+LIVE_API_KEY = "yourliveapikey"
+LIVE_SECRET_KEY = "yourlivesecretkey"
+
+# The mail addresses and password for automated notifications
+sender_address = 'yourtradingbotemailaddress'
+sender_pass = 'yourtradingbotemailpassword'
+receiver_address = 'yourpersonalemailaddress'
